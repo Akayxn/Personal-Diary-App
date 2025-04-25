522680a518f553a6a2fa13196f62378764c8eb23
Yujan Bajagain <98070107+GamingClaus@users.noreply.github.com>
2025-04-25 18:57:14 +0545

    Finished adding the read feature as well as the delete feature

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import  java.time.*;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
@@ -9,41 +11,38 @@ public class App {
     static BufferedWriter writer;
     static String filePath = "src/main/java/";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InputMismatchException {
         LocalDate localDate = LocalDate.now(); //getting today's date.
         // For taking in inputs from the users and showing the outputs when needed.
 
-        try{
-
-            int choice;
             do{
-                UserMenu();
-                System.out.print("What do you want to do?:");
-                choice = scanner.nextInt();
-                scanner.nextLine(); //taking up the extra space created
-                switch (choice){
-                    case 1:
-                        writeDiary(localDate);
-                        break;
-                    case 2:
-                        readDiary();
-                        break;
-                    case 3:
-                        deleteDiary();
-                        break;
-                    case 4:
-                        System.out.println("Diary Closed!");
-                        System.exit(444);
-                    default:
-                        System.out.println("Invalid! Enter a correct option");
+                try{
+                    int choice;
+                    UserMenu();
+                    System.out.print("What do you want to do?:");
+                    choice = scanner.nextInt();
+                    scanner.nextLine(); //taking up the extra space created
+                    switch (choice){
+                        case 1:
+                            writeDiary(localDate);
+                            break;
+                        case 2:
+                            readDiary();
+                            break;
+                        case 3:
+                            deleteDiary();
+                            break;
+                        case 4:
+                            System.out.println("Diary Closed!");
+                            System.exit(444);
+                        default:
+                            System.out.println("Invalid! Enter a correct option");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Please enter the number for choices! ");
+                    scanner.nextLine();
                 }
             }while(true);
-
-
-        }catch (Exception e){
-            System.out.println("Error:"+ e);
-        }
-
     }
 
 
@@ -54,7 +53,7 @@ public class App {
         try{
             String userInput = "";
             if(file.createNewFile()) {
-                System.out.println("Creating a file with the current date.");
+                System.out.println("Creating a page with the current date.");
             }
             else {
                 System.out.println("You have already made a Diary today.");
@@ -74,7 +73,7 @@ public class App {
                 writer.newLine();
                 }
             writer.write("By: "+name);
-            System.out.println("File written to " + file);
+            System.out.println("Diary written in: " + file);
             writer.close();
             } catch (IOException e) {
                 throw new RuntimeException(e);
@@ -83,31 +82,27 @@ public class App {
 
     private static void readDiary() {
         file = new File(filePath);
-        String[] files = file.list();
+        ArrayList<String> fileArrayList = new ArrayList<>();
 
-        if(files == null || files.length == 0){
-            System.out.println("No Diaries right now, create one to read it!");
-            return;
-        }
+        fileArrayList = listFiles();
 
-        System.out.print("Files:");
-        System.out.println("-----------------------------------------");
-        for(String fileName:files){
-            System.out.println(fileName);
-        }
-        System.out.println("-----------------------------------------");
-
-        System.out.println("Choose a Diary Date to read:");
-        String userChoice = scanner.nextLine();
+        System.out.print("Choose a Diary Date to read:");
+        String userChoice = scanner.nextLine().trim();
 
         try{
-            if(userChoice.equals(file.getName())){
+            String line;
+
+            if(fileArrayList.contains(userChoice)){
                 String fullFilePath = filePath +"/" + userChoice;
                 reader = new BufferedReader(new FileReader(fullFilePath));
-                while((reader.readLine()!=null)){
-                    System.out.println(reader.readLine());
+                while((line = reader.readLine())!=null){
+                    System.out.println(line);
                 }
                 reader.close();
+            }
+            else{
+                System.out.println("No Diary Page with that date exists! Please try again.");
+                return;
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -115,8 +110,55 @@ public class App {
     }
 
     private static void deleteDiary() {
-        System.out.println("Writing");
+        file = new File(filePath);
+        ArrayList<String> fileArrayList = new ArrayList<>();
+
+        fileArrayList = listFiles();
+
+        System.out.print("Choose a Diary Date to delete:");
+        String userChoice = scanner.nextLine().trim();
+
+        try{
+            String line;
+            if(fileArrayList.contains(userChoice)) {
+                File fullFilePath = new File(file + "/" + userChoice);
+                if (fullFilePath.delete()) {
+                    System.out.println("Diary Page with the date " + userChoice + " scrapped successfully.");
+                }
+            }
+            else{
+                System.out.println("No Diary Page with that Date exists! Please try again.");
+                return;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    private static ArrayList<String> listFiles(){
+        file = new File(filePath);
+        String[] files = file.list();
+
+        if(files == null || files.length == 0){
+            System.out.println("No Diaries right now, create one to read it!");
+            return null;
+        }
+
+        System.out.println("Files:");
+        System.out.println("-----------------------------------------");
+        ArrayList<String> fileNamesArray = new ArrayList<>();
+        for(String fileName:files){
+            int i = 1 ;
+            if(fileName.startsWith("20")){ // only shows diaries from that directory.
+                System.out.println(String.valueOf(i) + ". " +fileName);
+                fileNamesArray.add(fileName);
+            }
+            i++;
+        }
+        System.out.println("-----------------------------------------");
+        return fileNamesArray;
+    }
+
 
     private static void UserMenu(){
         System.out.println("===============MENU=================");
